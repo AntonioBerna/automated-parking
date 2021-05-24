02682693f4ada874624cedb94e8d93e744899ab0
Antonio Bernardini <60107274+AntonioBerna@users.noreply.github.com>
2021-05-24 19:17:41 +0200

    Add files via upload

--- TelegramBot/bot.py
+++ TelegramBot/bot.py
@@ -3,31 +3,34 @@ from telebot import types
 import telebot
 import json
 
-
 parking = Parking()
 db = json.load(open("db.json"))
 bot = telebot.TeleBot(db["token"])
 print("Bot in esecuzione.")
 
-markup = types.ReplyKeyboardMarkup()
+markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 state_button = types.KeyboardButton(db["name_state_button"])
-markup.row(state_button)
+help_button = types.KeyboardButton(db["name_help_button"])
+markup.row(state_button, help_button)
 
 @bot.message_handler(commands=["start"])
 def send_start(message):
     bot.send_message(message.chat.id, text=db["start_text"], reply_markup=markup)
 
 @bot.message_handler(commands=["state"])
-# message.text[1:len(message.text)-1]
-@bot.message_handler(func=lambda message: message.text == db["name_state_button"])
+@bot.message_handler(func=lambda message: message.text == db["name_state_button"]) # message.text[1:len(message.text)-1]
 def send_state(message):
     seats = parking.getSeats()
-    if len(str(seats)) == 1:
-        if seats > 0:
-            bot.send_message(message.chat.id, text=f"Posti Liberi: {seats}")
-        else:
-            bot.send_message(message.chat.id, text="Il Parcheggio è pieno!")
+    total_seats = parking.getTotalSeats()
+
+    if len(str(seats)) == 1 and seats > 0:
+        bot.send_message(message.chat.id, text=f"Informazioni Parcheggio\n\n{parking.getMessage()}")
     else:
-        bot.send_message(message.chat.id, text="Si è verificato un errore...Riprova!")
+        bot.send_message(message.chat.id, text=f"Informazioni Parcheggio\n\n{parking.getMessage()}")
+
+@bot.message_handler(commands=["help"])
+@bot.message_handler(func=lambda message: message.text == db["name_help_button"])
+def send_help(message):
+    bot.send_message(message.chat.id, text=db["help_text"])
 
 bot.polling()
--- TelegramBot/parking.py
+++ TelegramBot/parking.py
@@ -3,9 +3,28 @@ import time
 
 class Parking:
     def __init__(self):
-        self.arduino = serial.Serial(port="/dev/ttyACM0", baudrate=9600, timeout=0)
+        try:
+            self.arduino = serial.Serial(port="/dev/ttyACM0", baudrate=9600, timeout=0)
+        except serial.serialutil.SerialException:
+            print("Non è stata rilevata alcuna scheda Arduino...Riprova più tardi!")
+            exit()
+        
         self.delay = 2
+        self.seats = -1
+        self.total_seats = 5
+        self.hour = time.strftime("%H:%M:%S")
+        self.date = time.strftime("%d/%m/%Y")
+
+    def getTotalSeats(self):
+        return self.total_seats
     
     def getSeats(self):
         time.sleep(self.delay)
-        return int(self.arduino.readline().decode("utf-8").strip())
+        self.seats = int(self.arduino.readline().decode("utf-8").strip())
+        return self.seats
+
+    def getMessage(self):
+        if self.seats > 0:
+            return f"Posti Disponibili: {self.seats}\nPosti Totali: {self.total_seats}\n\nUltimo Aggiornamento:\n{self.date} {self.hour}"
+        else:
+            return f"Il parcheggio è pieno!\nRiprova più tardi...\n\nUltimo Aggiornamento:\n{self.date} {self.hour}"
