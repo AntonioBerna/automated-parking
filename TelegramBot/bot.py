0773fd530c63e658cf033563be3a05fecc3579aa
Antonio Bernardini <60107274+AntonioBerna@users.noreply.github.com>
2021-05-25 20:15:49 +0200

    Add files via upload

--- TelegramBot/bot.py
+++ TelegramBot/bot.py
@@ -1,22 +1,24 @@
-from parking import Parking
+from parking import Parking, Reservation, QRCodeMaker
 from telebot import types
 import telebot
 import json
 
+data_user = {}
 parking = Parking()
+user = Reservation(data_user)
 db = json.load(open("db.json"))
-bot = telebot.TeleBot(db["token"])
+bot = telebot.TeleBot(db["token"], parse_mode="HTML")
 print("Bot in esecuzione.")
 
-markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-state_button = types.KeyboardButton(db["name_state_button"])
-help_button = types.KeyboardButton(db["name_help_button"])
-reservation_button = types.KeyboardButton(db["name_reservation_button"])
-markup.row(state_button, reservation_button)
-markup.row(help_button)
 
 @bot.message_handler(commands=["start"])
 def send_start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    state_button = types.KeyboardButton(db["name_state_button"])
+    help_button = types.KeyboardButton(db["name_help_button"])
+    reservation_button = types.KeyboardButton(db["name_reservation_button"])
+    markup.row(state_button, reservation_button)
+    markup.row(help_button)
     bot.send_message(message.chat.id, text=db["start_text"], reply_markup=markup)
 
 # message.text[1:len(message.text)-1] if I decide to use emoji
@@ -29,9 +31,45 @@ def send_state(message):
 @bot.message_handler(commands=["reservation"])
 @bot.message_handler(func=lambda message: message.text == db["name_reservation_button"])
 def send_reservation(message):
-    bot.send_message(message.chat.id, text="Inserisci il tuo nome:")
-    bot.send_message(message.chat.id, text="Inserisci la data di prenotazione:")
-    bot.send_message(message.chat.id, text="Inserisci l'orario:")
+    if parking.getSeats() > 0:
+        msg = bot.send_message(message.chat.id, text="Inserisci il tuo nome:")
+        bot.register_next_step_handler(msg, process_name_step)
+    else:
+        bot.send_message(message.chat.id, text=f"Il parcheggio è pieno!\nRiprova più tardi...")
+
+def process_name_step(message):
+    try:
+        chat_id = message.chat.id
+        name = message.text
+        user.name = name
+        data_user["name"] = user.name
+
+        msg = bot.send_message(chat_id, text="Per quando vuoi prenotare il parcheggio? (gg/mm/aaaa)")
+        bot.register_next_step_handler(msg, process_date_step)
+    except Exception as e:
+        print("Si è verificato un errore... Riprova più tardi!")
+        bot.send_message(chat_id, "Si è verificato un errore... Riprova più tardi!")
+
+def process_date_step(message):
+    try:
+        chat_id = message.chat.id
+        date = message.text
+        date_format = "%d/%m/%Y"
+        if user.checkDate(date, date_format):
+            identifier = user.randomCode()
+            qr_code = QRCodeMaker(identifier)
+            user.date = date
+            data_user["date"] = user.date
+            
+            user.addReservation(chat_id, data_user)
+
+            bot.send_photo(chat_id, photo=qr_code.make(), caption=f"Prenotazione effettuata!\n\nNome: {user.name}\nData prenotazione: {user.date}\nID: {identifier}")
+        else:
+            msg = bot.send_message(chat_id, text="Data inserita non è valida... Riprova!\n\nPer quando vuoi prenotare il parcheggio? (gg/mm/aaaa)")
+            bot.register_next_step_handler(msg, process_date_step)
+    except Exception as e:
+        print(f"Si è verificato un errore... Riprova più tardi!\n{e}")
+        bot.send_message(chat_id, "Si è verificato un errore... Riprova più tardi!")
 
 @bot.message_handler(commands=["help"])
 @bot.message_handler(func=lambda message: message.text == db["name_help_button"])
--- TelegramBot/parking.py
+++ TelegramBot/parking.py
@@ -1,5 +1,11 @@
+from random import *
+import datetime
+import string
 import serial
+import qrcode
 import time
+import json
+
 
 class Parking:
     def __init__(self):
@@ -8,13 +14,9 @@ class Parking:
         except serial.serialutil.SerialException:
             print("Non è stata rilevata alcuna scheda Arduino... Riprova più tardi!")
             exit()
-        
         self.delay = 2
-        self.seats = -1
         self.digits = 1 # 2 ?!
         self.total_seats = 5
-        self.hour = time.strftime("%H:%M:%S")
-        self.date = time.strftime("%d/%m/%Y")
     
     def giveSeats(self, seats):
         self.arduino.write(str(seats).encode("ascii"))
@@ -31,10 +33,53 @@ class Parking:
 
     def getMessage(self):
         current_seats = self.getSeats()
+        hour = time.strftime("%H:%M:%S")
+        date = time.strftime("%d/%m/%Y")
         if len(str(current_seats)) == self.digits:
             if current_seats > 0:
-                return f"Posti Disponibili: {current_seats}\nPosti Totali: {self.total_seats}\n\nUltimo Aggiornamento:\n{self.date} {self.hour}"
+                return f"Posti Disponibili: {current_seats}\nPosti Totali: {self.total_seats}\n\nUltimo Aggiornamento:\n{date} {hour}"
             else:
-                return f"Il parcheggio è pieno!\nRiprova più tardi...\n\nUltimo Aggiornamento:\n{self.date} {self.hour}"
+                return f"Il parcheggio è pieno!\nRiprova più tardi...\n\nUltimo Aggiornamento:\n{date} {hour}"
         else:
             return "Si è verificato un errore... Riprova più tardi!"
+
+
+class Reservation:
+    def __init__(self, data_user):
+        self.name = None
+        self.date = None
+        self.hour = None
+        self.current_date = time.strftime("%d/%m/%Y")
+
+        self.reservation = json.load(open("reservation.json"))
+        self.matrix = []
+    
+    def addReservation(self, chat_id, data_user):
+        self.matrix.append(data_user)
+        print(self.matrix)
+        self.reservation[chat_id] = self.matrix
+        with open("reservation.json", "w") as json_file:
+            json.dump(self.reservation, json_file, indent=2)
+    
+    def checkDate(self, date, date_format):
+        if datetime.datetime.strptime(date, date_format):
+            day, month, year = tuple(date.split("/"))
+            current_day, current_month, current_year = tuple(self.current_date.split("/"))
+            if (year == current_year and month == current_month  and day == current_day) or \
+            (year == current_year and month == current_month  and day > current_day) or \
+            (year == current_year and month > current_month) or \
+            (year > current_year):
+                return 1
+        else:
+            return 0
+        
+    def randomCode(self):
+        return "".join(choice(string.digits) for x in range(8))
+
+
+class QRCodeMaker:
+    def __init__(self, text):
+        self.text = " ".join(text)
+    
+    def make(self):
+        return qrcode.make(self.text).get_image()
