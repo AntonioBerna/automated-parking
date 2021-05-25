6d77006a938dd00cad85d11d88a5adb2f2f33951
Antonio Bernardini <60107274+AntonioBerna@users.noreply.github.com>
2021-05-25 11:09:05 +0200

    Add files via upload

--- TelegramBot/bot.py
+++ TelegramBot/bot.py
@@ -11,22 +11,27 @@ print("Bot in esecuzione.")
 markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 state_button = types.KeyboardButton(db["name_state_button"])
 help_button = types.KeyboardButton(db["name_help_button"])
-markup.row(state_button, help_button)
+reservation_button = types.KeyboardButton(db["name_reservation_button"])
+markup.row(state_button, reservation_button)
+markup.row(help_button)
 
 @bot.message_handler(commands=["start"])
 def send_start(message):
     bot.send_message(message.chat.id, text=db["start_text"], reply_markup=markup)
 
+# message.text[1:len(message.text)-1] if I decide to use emoji
 @bot.message_handler(commands=["state"])
-@bot.message_handler(func=lambda message: message.text == db["name_state_button"]) # message.text[1:len(message.text)-1]
+@bot.message_handler(func=lambda message: message.text == db["name_state_button"])
 def send_state(message):
-    seats = parking.getSeats()
-    total_seats = parking.getTotalSeats()
+    bot.send_message(message.chat.id, text=f"Informazioni Parcheggio\n\n{parking.getMessage()}")
+    parking.giveSeats(4)
 
-    if len(str(seats)) == 1 and seats > 0:
-        bot.send_message(message.chat.id, text=f"Informazioni Parcheggio\n\n{parking.getMessage()}")
-    else:
-        bot.send_message(message.chat.id, text=f"Informazioni Parcheggio\n\n{parking.getMessage()}")
+@bot.message_handler(commands=["reservation"])
+@bot.message_handler(func=lambda message: message.text == db["name_reservation_button"])
+def send_reservation(message):
+    bot.send_message(message.chat.id, text="Inserisci il tuo nome:")
+    bot.send_message(message.chat.id, text="Inserisci la data di prenotazione:")
+    bot.send_message(message.chat.id, text="Inserisci l'orario:")
 
 @bot.message_handler(commands=["help"])
 @bot.message_handler(func=lambda message: message.text == db["name_help_button"])
--- TelegramBot/parking.py
+++ TelegramBot/parking.py
@@ -6,25 +6,35 @@ class Parking:
         try:
             self.arduino = serial.Serial(port="/dev/ttyACM0", baudrate=9600, timeout=0)
         except serial.serialutil.SerialException:
-            print("Non è stata rilevata alcuna scheda Arduino...Riprova più tardi!")
+            print("Non è stata rilevata alcuna scheda Arduino... Riprova più tardi!")
             exit()
         
         self.delay = 2
         self.seats = -1
+        self.digits = 1 # 2 ?!
         self.total_seats = 5
         self.hour = time.strftime("%H:%M:%S")
         self.date = time.strftime("%d/%m/%Y")
-
-    def getTotalSeats(self):
-        return self.total_seats
+    
+    def giveSeats(self, seats):
+        self.arduino.write(str(seats).encode("ascii"))
     
     def getSeats(self):
-        time.sleep(self.delay)
-        self.seats = int(self.arduino.readline().decode("utf-8").strip())
-        return self.seats
+        try:
+            time.sleep(self.delay)
+            self.seats = int(self.arduino.readline().decode("utf-8").strip())
+            return self.seats
+        except Exception as e:
+            print(f"Errore 123... Riprova più tardi!\n{e}")
+            return 123
+            exit()
 
     def getMessage(self):
-        if self.seats > 0:
-            return f"Posti Disponibili: {self.seats}\nPosti Totali: {self.total_seats}\n\nUltimo Aggiornamento:\n{self.date} {self.hour}"
+        current_seats = self.getSeats()
+        if len(str(current_seats)) == self.digits:
+            if current_seats > 0:
+                return f"Posti Disponibili: {current_seats}\nPosti Totali: {self.total_seats}\n\nUltimo Aggiornamento:\n{self.date} {self.hour}"
+            else:
+                return f"Il parcheggio è pieno!\nRiprova più tardi...\n\nUltimo Aggiornamento:\n{self.date} {self.hour}"
         else:
-            return f"Il parcheggio è pieno!\nRiprova più tardi...\n\nUltimo Aggiornamento:\n{self.date} {self.hour}"
+            return "Si è verificato un errore... Riprova più tardi!"
