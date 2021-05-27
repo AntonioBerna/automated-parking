0f0358f6d7b840d7ea7e2530e53822f58366defb
Antonio Bernardini <60107274+AntonioBerna@users.noreply.github.com>
2021-05-27 02:03:30 +0200

    Add files via upload

--- TelegramBot/reservation.py
+++ TelegramBot/reservation.py
@@ -6,7 +6,7 @@ import time
 
 
 class Reservation:
-    def __init__(self, data_user):
+    def __init__(self):
         self.name = None
         self.date = None
         self.hour = None
@@ -17,14 +17,14 @@ class Reservation:
         
         if chat_id not in reservation.keys():
             reservation[chat_id] = []
-        reservation[chat_id].append(date_user)
+        reservation[chat_id].append(data_user)
 
         with open("reservation.json", "w") as file:
-            json.dump(self.reservation, file, indent=2)
+            json.dump(reservation, file, indent=2)
     
     def checkDate(self, date):
         try:
-            datetime.datetime.strptime(date, "%d/%m/%Y"):
+            datetime.datetime.strptime(date, "%d/%m/%Y")
             day, month, year = tuple(date.split("/"))
             current_day, current_month, current_year = tuple(self.current_date.split("/"))
 
